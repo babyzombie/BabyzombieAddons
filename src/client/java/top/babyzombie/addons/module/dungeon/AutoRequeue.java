a60089ee0941eee766056a2495c591bc314aece6
babyzombie <2369574237@qq.com>
2026-06-30 01:55:27 +0800

    - 修复地牢/kuudra重开时会空指针

--- src/client/java/top/babyzombie/addons/module/dungeon/AutoRequeue.java
+++ src/client/java/top/babyzombie/addons/module/dungeon/AutoRequeue.java
@@ -48,7 +48,7 @@ public final class AutoRequeue {
         var player = Minecraft.getInstance().player;
         if (player == null) return;
         if (cancelAutoJoin) return;
-        if (!PartyTracker.getInstance().getLeaderName().isEmpty() || !PartyTracker.getInstance().isSelfLeader()) return;
+        if (PartyTracker.getInstance().getLeaderName() != null || !PartyTracker.getInstance().isSelfLeader()) return;
         var loc = HypixelLocationTracker.getInstance();
         if (!loc.isInSkyblock() || (!loc.isInDungeon() && !loc.isInKuudra())) return;
 
