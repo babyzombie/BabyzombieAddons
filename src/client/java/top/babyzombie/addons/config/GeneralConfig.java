e764e75d3cdfa1798e87652e2d073e762589263a
babyzombie <2369574237@qq.com>
2026-09-10 03:13:29 +0800

    * 移除手工 §→TextColor 转换:物品显示名/颜色直接以 legacy § 文本交给 MC 渲染解析
    - 删除 ChatUtils.toComponent/toLegacyString 手工解析器(§x 六位 hex 等),CUSTOM_NAME/lore 直接用 Component.literal 存 § 文本,渲染时自动解析
    - buildBracket 名字颜色改为取 legacy 文本的颜色码前缀(leadingColorCode),不再解析成 TextColor 再写回 Style
    - GeneralConfig 内部枚举引用补全限名(26.2 merge 遗留)

--- src/client/java/top/babyzombie/addons/config/GeneralConfig.java
+++ src/client/java/top/babyzombie/addons/config/GeneralConfig.java
@@ -104,11 +104,11 @@ public class GeneralConfig {
         @Expose @ConfigOption(name = "config.babyzombieaddons.option.chatInContainer", desc = "config.babyzombieaddons.option.chatInContainer.desc") @ConfigEditorBoolean @SearchTag("chat")
         public boolean chatInContainer = false;
         @Expose @ConfigOption(name = "config.babyzombieaddons.option.chatSendItemMode", desc = "config.babyzombieaddons.option.chatSendItemMode.desc") @ConfigEditorDropdown @SearchTag("chat") @SearchTag("item") @SearchTag("send")
-        public ChatSendItemMode chatSendItemMode = ChatSendItemMode.ID_NAME;
+        public ModConfig.ChatSendItemMode chatSendItemMode = ModConfig.ChatSendItemMode.ID_NAME;
         @Expose @ConfigOption(name = "config.babyzombieaddons.option.chatItemDisplay", desc = "config.babyzombieaddons.option.chatItemDisplay.desc") @ConfigEditorBoolean @SearchTag("chat") @SearchTag("item")
         public boolean chatItemDisplay = true;
         @Expose @ConfigOption(name = "config.babyzombieaddons.option.chatItemDisplayMode", desc = "config.babyzombieaddons.option.chatItemDisplayMode.desc") @ConfigEditorDropdown @SearchTag("chat") @SearchTag("item")
-        public ChatItemDisplayMode chatItemDisplayMode = ChatItemDisplayMode.ICON_NAME;
+        public ModConfig.ChatItemDisplayMode chatItemDisplayMode = ModConfig.ChatItemDisplayMode.ICON_NAME;
     }
 
     public static class AutoReconnect {
